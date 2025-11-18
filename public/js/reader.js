ce0ee5182ba75b6886c2757c92db9a144fa01442
yoraldo <yomcdonald69@gmail.com>
2025-11-18 03:16:05 -0800

    Center images when viewport is taller than images

--- public/js/reader.js
+++ public/js/reader.js
@@ -693,7 +693,12 @@ Reader.goToPage = function (page) {
     Reader.showingSinglePage = false;
 
     if (Reader.infiniteScroll) {
-        $("#display img").get(Reader.currentPage).scrollIntoView({ block: 'nearest' });
+        let img = $("#display img").get(Reader.currentPage);
+        if (img.height > window.innerHeight) {
+            img.scrollIntoView({ block: 'nearest' });
+        } else {
+            img.scrollIntoView({ block: 'center' });
+        }
     } else {
         $("#img_doublepage").attr("src", "");
         $("#display").removeClass("double-mode");
