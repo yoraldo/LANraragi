4a684f1eb87b3041efbd7e7e671248afedf2404d
yoraldo <yomcdonald69@gmail.com>
2026-05-31 15:58:43 -0700

    Stay awake when autoscrolling

--- public/js/reader.js
+++ public/js/reader.js
@@ -51,6 +51,7 @@ let markersVisible = false;
 let markers = [];
 let overlayFiltered = false;
 let pageNaviState = true;
+let wakeLock = null;
 
 export function initializeAll(trackProgressLocally, authenticateProgress) {
     state.trackProgressLocally = trackProgressLocally;
@@ -1567,6 +1568,8 @@ function startAutoNextPage() {
         autoNextPageCountdown -= 1;
         aEls.text(autoNextPageCountdown);
     }, 1000);
+
+    requestWakeLock();
 }
 
 function stopAutoNextPage() {
@@ -1574,6 +1577,8 @@ function stopAutoNextPage() {
     clearInterval(autoNextPageCountdownTaskId);
     $(".toggle-auto-next-page").addClass("fa-stopwatch");
     $(".toggle-auto-next-page").text("");
+
+    releaseWakeLock();
 }
 
 function toggleAutoNextPage() {
@@ -1957,3 +1962,36 @@ jQuery(() => {
         }
     });
 });
+
+async function requestWakeLock() {
+    if (wakeLock !== null) {
+        return;
+    }
+    if (!("wakeLock" in navigator)) {
+        console.error("Wake Lock API is not available.");
+        return;
+    }
+
+    try {
+        wakeLock = await navigator.wakeLock.request();
+
+        wakeLock.addEventListener("release", () => {
+            wakeLock = null;
+        });
+    } catch (err) {
+        console.error("Error acquiring wake lock:", err);
+    }
+}
+// Browser releases the wake lock when the tab is hidden so we need to reacquire it
+document.addEventListener("visibilitychange", async () => {
+    if (autoNextPage && wakeLock !== null && document.visibilityState === "visible") {
+        await requestWakeLock();
+    }
+});
+
+function releaseWakeLock() {
+    if (wakeLock !== null) {
+        wakeLock.release();
+    }
+}
+
